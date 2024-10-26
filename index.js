adfb2ee58fe76395100b053444f9c23ca23108f2
Miko Clark Cruz <110460613+Mikouxei@users.noreply.github.com>
2024-10-26 19:18:43 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+/* -------------------------- HEADER (FLOAT) -------------------------- */
+
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", function() {
+  header.classList.toggle("float", window.scrollY > 80);
+});
+
+/* -------------------------- MENU ICON / NAVIGATION LIST -------------------------- */
+function xmenu(x) {
+  x.classList.toggle("change");
+  document.querySelector(".navlist").classList.toggle("active");
+}
+
+/* -------------------------- GRADIENT MOUSE POINTER -------------------------- */
+const cursor = document.querySelector('.gradient');
+
+document.addEventListener('mousemove', function(e) {
+    cursor.style.left = `${e.clientX - 250}px`;
+    cursor.style.top = `${e.clientY - 250}px`; 
+});
